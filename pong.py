4625a429c58709bdd1f37a57ee2678dc8ad8cd47
BulatBatraev <batraevbe@gmail.com>
2024-01-23 19:01:12 +0400

    Доделал спрайт игрока и исправил некоторые недочеты.

--- pong.py
+++ pong.py
@@ -1,9 +1,31 @@
 import pygame as pg
+from sprites import Player
 
 
 def main():
+    width = 800
+    height = 600
+    fps = 60
     pg.init()
+    display = pg.display.set_mode((width, height))
+    pg.display.set_caption("Pong")
+    clock = pg.time.Clock()
+    all_sprites = pg.sprite.Group()
+    player = Player(width, height)
+    all_sprites.add(player)
+
+    run = True
+    while run:
+        clock.tick(fps)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+        all_sprites.update()
+        display.fill((255, 255, 255))
+        all_sprites.draw(display)
+        pg.display.flip()
 
 
 if __name__ == "__main__":
     main()
+    pg.quit()
--- sprites.py
+++ sprites.py
@@ -7,17 +7,19 @@ class Player(pg.sprite.Sprite):
     color = (0, 0, 150)
     speed_y = 5
 
-    def __init__(self, center_x, center_y, display_width, display_height):
+    def __init__(self, display_width, display_height):
         super().__init__()
         self.image = pg.Surface((self.width, self.height))
         self.image.fill(self.color)
         self.rect = self.image.get_rect()
-        self.center = (center_x, center_y)
+        self.rect.center = (display_width/8, display_height/2)
         self.display_width = display_width
         self.display_height = display_height
 
-
     def update(self):
         keys = pg.key.get_pressed()
-        if keys[pg.K_s]:
+        if keys[pg.K_s] and self.rect.y + self.rect.height <= self.display_height:
+            self.rect.y += self.speed_y
+        if keys[pg.K_w] and self.rect.y >= 0:
+            self.rect.y += self.speed_y * -1
 
