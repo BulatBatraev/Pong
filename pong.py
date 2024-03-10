715d481044d850e0940f70d6b79c2b93320583da
BulatBatraev <batraevbe@gmail.com>
2024-03-10 19:34:05 +0400

    Баги исправил, почти все, осталась графика и экран окончания.

--- pong.py
+++ pong.py
@@ -1,6 +1,7 @@
 import pygame as pg
 from sprites import Player
 from sprites import Ball
+import time
 
 
 def main():
@@ -16,6 +17,14 @@ def main():
     ball = Ball(width, height)
     all_sprites.add(player)
     all_sprites.add(ball)
+    font_name = pg.font.match_font('arial')
+
+    def draw_text(surf, text, size, x, y):
+        font = pg.font.Font(font_name, size)
+        text_surface = font.render(text, False, (0, 255, 0))
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        surf.blit(text_surface, text_rect)
 
     run = True
     while run:
@@ -23,7 +32,12 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
-        all_sprites.update()
+        if ball.game_over:
+            draw_text(display, "GameOver", 60, width/4, height/2)
+            time.sleep(3)
+            run = False
+        player.update()
+        ball.update(player.rect.x, player.rect.y)
         display.fill((255, 255, 255))
         all_sprites.draw(display)
         pg.display.flip()
--- sprites.py
+++ sprites.py
@@ -29,7 +29,8 @@ class Ball(pg.sprite.Sprite):
     height = 30
     color = (200, 0, 0)
     speed_x = 5
-    speed_y = -5
+    speed_y = 5
+    game_over = False
 
     def __init__(self, display_width, display_height):
         super().__init__()
@@ -39,20 +40,19 @@ class Ball(pg.sprite.Sprite):
         self.rect.center = (display_width/2, display_height/2)
         self.display_width = display_width
         self.display_height = display_height
+        self.player = Player(self.display_width, self.display_height)
 
-    def update(self):
+    def update(self, player_x, player_y):
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
         if self.rect.x == 0 or self.rect.x + self.rect.width == self.display_width:
             self.speed_x *= -1
         if self.rect.y == 0 or self.rect.y + self.rect.height == self.display_height:
             self.speed_y *= -1
-
-
-
-
-
-
-
+        if self.rect.x == player_x + self.player.rect.width and self.rect.y + self.rect.height > player_y\
+                and self.rect.y < player_y + self.player.rect.height:
+            self.speed_x *= -1
+        if self.rect.x == 5:
+            self.game_over = True
 
 
